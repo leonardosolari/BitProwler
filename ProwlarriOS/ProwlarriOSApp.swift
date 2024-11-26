5c326c582dbf059643cfa3692c8b6e2b86e6d6b4
Leonardo Solari <leonardosolari67@gmail.com>
2024-11-26 21:08:15 +0100

    Initial Commit

---
+++ ProwlarriOS/ProwlarriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProwlarriOSApp.swift
+//  ProwlarriOS
+//
+//  Created by Leonardo Solari on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProwlarriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProwlarriOSTests/ProwlarriOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProwlarriOSTests.swift
+//  ProwlarriOSTests
+//
+//  Created by Leonardo Solari on 26/11/24.
+//
+
+import Testing
+@testable import ProwlarriOS
+
+struct ProwlarriOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
