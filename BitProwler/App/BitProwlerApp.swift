fdef8c5c87afa0b349cf79221b90e794e7feca8b
Leonardo Solari <leonardosolari67@gmail.com>
2025-08-02 16:55:36 +0200

    Update BitProwlerApp.swift

--- BitProwler/App/BitProwlerApp.swift
+++ BitProwler/App/BitProwlerApp.swift
@@ -2,9 +2,17 @@ import SwiftUI
 
 @main
 struct BitProwlerApp: App {
+    @StateObject private var container = AppContainer()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(container)
+                .environmentObject(container.prowlarrManager)
+                .environmentObject(container.qbittorrentManager)
+                .environmentObject(container.recentPathsManager)
+                .environmentObject(container.searchHistoryManager)
+                .environmentObject(container.filterViewModel)
         }
     }
 }
