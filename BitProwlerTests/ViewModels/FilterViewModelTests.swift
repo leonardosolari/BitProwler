c06f6465deb5b2f8d72e64d74a7c421d34fe0ee3
Leonardo Solari <leonardosolari67@gmail.com>
2025-12-02 18:38:27 +0100

    test: fix search filters unit test

--- BitProwlerTests/ViewModels/FilterViewModelTests.swift
+++ BitProwlerTests/ViewModels/FilterViewModelTests.swift
@@ -4,23 +4,26 @@ import Foundation
 
 struct FilterViewModelTests {
     
-    var viewModel: FilterViewModel
-    var sampleResults: [TorrentResult]
+    private let userDefaultsSuiteName = "FilterViewModelTests"
+    private let sampleResults: [TorrentResult]
     
     init() {
-        let testDefaults = UserDefaults(suiteName: "FilterViewModelTests")!
-        testDefaults.removePersistentDomain(forName: "FilterViewModelTests")
-        
-        viewModel = FilterViewModel(userDefaults: testDefaults)
-        
-        sampleResults = [
+        self.sampleResults = [
             TorrentResult(id: "1", title: "Ubuntu 24.04 Desktop", size: 0, seeders: 0, leechers: 0, downloadUrl: nil, magnetUrl: nil, infoUrl: nil, indexer: "", publishDate: ""),
             TorrentResult(id: "2", title: "Ubuntu Server", size: 0, seeders: 0, leechers: 0, downloadUrl: nil, magnetUrl: nil, infoUrl: nil, indexer: "", publishDate: ""),
             TorrentResult(id: "3", title: "Debian NetInst", size: 0, seeders: 0, leechers: 0, downloadUrl: nil, magnetUrl: nil, infoUrl: nil, indexer: "", publishDate: "")
         ]
     }
+
+    private func createCleanViewModel() -> FilterViewModel {
+        let defaults = UserDefaults(suiteName: userDefaultsSuiteName)!
+        defaults.removePersistentDomain(forName: userDefaultsSuiteName)
+        return FilterViewModel(userDefaults: defaults)
+    }
     
     @Test func addAndToggleFilter() {
+        let viewModel = createCleanViewModel()
+
         let filter = TorrentFilter(name: "Ubuntu", keyword: "ubuntu")
         viewModel.addFilter(filter)
         
@@ -28,10 +31,13 @@ struct FilterViewModelTests {
         #expect(viewModel.filters.first?.isEnabled == true)
         
         viewModel.toggleFilter(filter)
+        
         #expect(viewModel.filters.first?.isEnabled == false)
     }
     
     @Test func filterLogicAND() {
+        let viewModel = createCleanViewModel()
+
         viewModel.filterLogic = .and
         viewModel.addFilter(TorrentFilter(name: "Ubuntu", keyword: "Ubuntu"))
         viewModel.addFilter(TorrentFilter(name: "Desktop", keyword: "Desktop"))
@@ -43,6 +49,8 @@ struct FilterViewModelTests {
     }
     
     @Test func filterLogicOR() {
+        let viewModel = createCleanViewModel()
+
         viewModel.filterLogic = .or
         viewModel.addFilter(TorrentFilter(name: "Server", keyword: "Server"))
         viewModel.addFilter(TorrentFilter(name: "Debian", keyword: "Debian"))
